858c0eea111fa4dc978442ce9cf53316e9a1e4ff
Maksim Rasporskyi <mraspors@lab3r8s9.42abudhabi.ae>
2022-01-03 15:54:56 +0400

    ft_strtrim done

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -9,71 +9,24 @@
 /*   Updated: 2021/12/23 15:08:16 by mraspors         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
+
 #include <stdlib.h>
+#include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	l;
-printf("aaaaaaaa copy\n");
-	l = 0;
-	i = 0;
-	while (i < size)
-	{
-		if (src[i] == '\0')
-			break ;
-		dst[i] = src[i];
-		l++;
-		i++;
-	}
-	dst[i] = '\0';
-	return (l);
-}
-
-char	*ft_strdup(const char *string)
-{
-	char	*str;
-	int		i;
-printf("aaaaaaaa strup\n");
-	i = 0;
-	while (string[i] != '\0')
-		i++;
-	str = (char *)malloc((i + 1) * sizeof(char));
-	i = 0;
-	while (string[i] != '\0')
-	{
-		str[i] = string[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-
-size_t	ft_len(const char *s)
-{
-	size_t	i;
-printf("aaaaaaaa len\n");
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-int			ft_checkset(char c, char const *set)
+int	ft_checkset(char c, char const *set)
 {
 	int		i;
 	i = 0;
 	while(set[i] != '\0')
 	{
-		printf("set: %d\n", i);
 		if(c == set[i])
 			return (1);
 		i++;
 	}
 	return 0;
 }
-int			ft_start(const char *s1, const char *set)
+
+int	ft_start(const char *s1, const char *set)
 {
 	size_t	i;
 
@@ -88,17 +41,17 @@ int			ft_start(const char *s1, const char *set)
 	return (i);
 }
 
-int			ft_end(const char *s1, const char *set)
+int	ft_end(const char *s1, const char *set)
 {
 	size_t	len;
 	size_t	i;
 
-	len = ft_len(s1);
+	len = ft_strlen(s1);
 	i = 0;
 	while(len - i >= 0)
 	{
-		if(ft_checkset(s1[len - i], set) == 0)
-			return (len - i);
+		if(ft_checkset(s1[len - i - 1], set) == 0)
+			return (len - i - 1);
 		i++;
 	}
 	return (len - i);
@@ -111,7 +64,7 @@ char		*ft_strtrim(char const *s1, char const *set)
 	char	*returnstr;
 
 	if (s1 == NULL)
-		//return (NULL);
+		return (NULL);
 	if (set == NULL)
 		return (ft_strdup(s1));
 	start = ft_start(s1, set);
@@ -125,13 +78,5 @@ char		*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	ft_strlcpy(returnstr, &s1[start], end - start + 1);
 	return (returnstr);
-}
-int main()
-{
-	char s[50] = "ABAcatcatcatBBAA";
-	printf("%s\n", s);
-	char set[3] = "AB";
-	printf("%s\n", ft_strtrim(s, set));
-	return 0;
 }
 
