ad3725d6d1be899eb47519a19534cbece3a4b1cc
Rozzini <Rozzinimr@gmail.com>
2022-01-06 15:38:56 +0300

    striteri done

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -11,3 +11,19 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+    unsigned int    i;
+
+    if (s == NULL || f == NULL)
+        return ;
+    i = 0;
+    while (s[i] != '\0')
+    {
+        f(i, &s[i]);
+        i++;
+    }
+}
