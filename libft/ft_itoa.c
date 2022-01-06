54d7b7e1e5e4eca3dc35bf81dc05f8e5c60a6fa2
Rozzini <Rozzinimr@gmail.com>
2022-01-06 15:27:46 +0300

    itoa and strmapi done

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -11,3 +11,54 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int		len(int n)
+{
+	int		len;
+
+	len = 0;
+	if (n < 0)
+	{
+		n = n * -1;
+		len++;
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char *str;
+	int		i;
+
+    if (n == -2147483648)
+        return ("-2147483648");
+	i = len(n);
+	str = (char*)malloc(sizeof(char) * (i + 1));
+    if (str == NULL)
+		return (NULL);
+	str[i--] = '\0';
+	if (n == 0)
+	{
+		str[0] = 48;
+		return (str);
+	}
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = n * -1;
+	}
+	while (n > 0)
+	{
+		str[i] = '0' + (n % 10);
+		n = n / 10;
+		i--;
+	}
+	return (str);
+}
---
+++ libft/ft_strmapi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mraspors <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/06 15:17:31 by mraspors          #+#    #+#             */
+/*   Updated: 2022/01/06 15:17:31 by mraspors         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+	int	result;
+
+	i = 0;
+	result = 0;
+	while (s[i++] != '\0')
+		result++;
+	return (result);
+}
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    unsigned int    i;
+    char    *str;
+
+    if (s == NULL)
+        return (NULL);
+    str = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+    if (str == NULL)
+        return (NULL);
+    i = 0;
+    while (s[i] != '\0')
+    {
+        str[i] = f(i, s[i]);
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
